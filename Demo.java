87f812fcf627390070b12aa4b7cdbd9d8022e34d
Vikraam17 <Vikraam1704@gmail.com>
2026-03-23 14:54:03 +0530

    Created Demo java File

---
+++ Demo.java
@@ -0,0 +1,12 @@
+public class Demo{
+	public static void main(String[]
+ args){
+	System.out.print("Hello");
+	}
+}
+class Demo1{
+	public static void main(String[]
+ args){
+	System.out.print("Hello");
+	}
+}
